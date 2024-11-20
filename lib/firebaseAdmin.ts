c745a730ef39785766e8e8840d065685b72f252b
judeclark19 <judeclark2019@gmail.com>
2024-11-19 21:47:56 -0500

    changing sdk environment variable for netlify

--- lib/firebaseAdmin.ts
+++ lib/firebaseAdmin.ts
@@ -1,10 +1,18 @@
 import * as admin from "firebase-admin";
-import serviceAccount from "../verkaikings-firebase-adminsdk.json"; // Adjust path if needed
+
+// Decode the Base64-encoded service account key from the environment variable
+const serviceAccount = process.env.NEXT_PUBLIC_FIREBASE_ADMIN_64
+  ? JSON.parse(
+      Buffer.from(process.env.NEXT_PUBLIC_FIREBASE_ADMIN_64, "base64").toString(
+        "utf-8"
+      )
+    )
+  : null;
 
 // Check if Firebase Admin app is already initialized
 if (!admin.apps.length) {
   const credential = serviceAccount
-    ? admin.credential.cert(serviceAccount as admin.ServiceAccount) // Use the local JSON file if available
+    ? admin.credential.cert(serviceAccount as admin.ServiceAccount) // Use the decoded service account if available
     : admin.credential.applicationDefault(); // Fallback to default credentials
 
   admin.initializeApp({
