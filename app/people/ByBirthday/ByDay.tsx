2f33c9e07cae7cf6ff3196e03dbd88875d4a5a48
judeclark19 <judeclark2019@gmail.com>
2024-12-28 22:04:06 -0500

    switchin up colors

--- app/people/ByBirthday/ByDay.tsx
+++ app/people/ByBirthday/ByDay.tsx
@@ -3,30 +3,32 @@ import { observer } from "mobx-react-lite";
 import UserListItem from "../UserListItem";
 import userList from "@/lib/UserList";
 
-const ByDay = observer(({ day, month }: { day: string; month: string }) => {
-  return (
-    <div key={day}>
-      <Typography
-        variant="h3"
-        sx={{
-          color: parseInt(day) % 2 !== 0 ? "primary.dark" : "secondary.dark"
-        }}
-      >
-        {day}
-      </Typography>
-      <List
-        sx={{
-          width: "100%",
-          bgcolor: "background.paper",
-          padding: 0
-        }}
-      >
-        {userList.usersByBirthday[month][day].map((user) => (
-          <UserListItem key={user.username} user={user} />
-        ))}
-      </List>
-    </div>
-  );
-});
+const ByDay = observer(
+  ({ day, month, color }: { day: string; month: string; color: string }) => {
+    return (
+      <div key={day}>
+        <Typography
+          variant="h3"
+          sx={{
+            color: color
+          }}
+        >
+          {day}
+        </Typography>
+        <List
+          sx={{
+            width: "100%",
+            bgcolor: "background.paper",
+            padding: 0
+          }}
+        >
+          {userList.usersByBirthday[month][day].map((user) => (
+            <UserListItem key={user.username} user={user} />
+          ))}
+        </List>
+      </div>
+    );
+  }
+);
 
 export default ByDay;
