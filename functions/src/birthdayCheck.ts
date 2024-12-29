400a8dc3779e79dc5c7dda8c28e69d01f074dca0
judeclark19 <judeclark2019@gmail.com>
2024-12-29 16:52:57 -0500

    send birthday admin message only if it's someone's bday

--- functions/src/birthdayCheck.ts
+++ functions/src/birthdayCheck.ts
@@ -1,4 +1,4 @@
-import {adminDb, notifyUsers} from "./utils";
+import { adminDb, notifyUsers } from "./utils";
 /**
  * Helper function to get the ordinal suffix of a number.
  *
@@ -28,18 +28,6 @@ export async function runBirthdayCheck() {
   console.log("Checking for birthdays", today);
 
   try {
-    // notify Jude
-    const judeMessage = {
-      title: "birthdayCheck function just ran",
-      body: "admin message",
-      url: null,
-    };
-
-    notifyUsers({
-      userIds: ["6pHYz7jcr7WoqoRWcnIXEn0Y1bm1"],
-      notification: judeMessage,
-    });
-
     const allUsersSnapshot = await adminDb.collection("users").get();
 
     for (const userDoc of allUsersSnapshot.docs) {
@@ -52,16 +40,28 @@ export async function runBirthdayCheck() {
       if (parseInt(month) === todayMonth && parseInt(day) === todayDay) {
         console.log("Send birthday notifiactions for user:", userData.username);
 
+        // notify Jude
+        const judeMessage = {
+          title: `birthday notifs sent for ${userData.username}`,
+          body: "admin message",
+          url: null
+        };
+
+        notifyUsers({
+          userIds: ["6pHYz7jcr7WoqoRWcnIXEn0Y1bm1"],
+          notification: judeMessage
+        });
+
         const age = todayYear - parseInt(year);
         const birthdayMessage = {
           title: `Happy ${getOrdinal(age)} Birthday!`,
           body: "Wishing you a fantastic day!",
-          url: null,
+          url: null
         };
 
         await notifyUsers({
           userIds: [userDoc.id],
-          notification: birthdayMessage,
+          notification: birthdayMessage
         });
 
         const otherUsers = allUsersSnapshot.docs
@@ -73,12 +73,12 @@ export async function runBirthdayCheck() {
           body: `It's ${userData.firstName} ${userData.lastName}'s ${getOrdinal(
             age
           )} birthday today.`,
-          url: `/profile/${userData.username}`,
+          url: `/profile/${userData.username}`
         };
 
         await notifyUsers({
           userIds: otherUsers,
-          notification: generalMessage,
+          notification: generalMessage
         });
       }
     }
