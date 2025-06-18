0f66e063e9bb2f896e115c7a1fc6d4a6d227b73b
judeclark19 <judeclark2019@gmail.com>
2025-06-18 11:47:00 +0200

    corrected colors for wow icon

--- components/Reactions/WowIcon.tsx
+++ components/Reactions/WowIcon.tsx
@@ -26,21 +26,21 @@ const WowIcon = ({
         cx="9"
         cy="9"
         r="1"
-        fill="#121212"
-        stroke="#121212"
+        fill={color ? "#121212" : "rgba(18, 18, 18, 0.7)"}
+        stroke={color ? "#121212" : "rgba(18, 18, 18, 0.7)"}
         strokeLinecap="round"
       />
       <circle // right eye
         cx="15"
         cy="9"
         r="1"
-        fill="#121212"
-        stroke="#121212"
+        fill={color ? "#121212" : "rgba(18, 18, 18, 0.7)"}
+        stroke={color ? "#121212" : "rgba(18, 18, 18, 0.7)"}
         strokeLinecap="round"
       />
       <path // mouth
         d="M15 15.5C15 16.8807 13.6569 18 12 18C10.3431 18 9 16.8807 9 15.5C9 14.1193 10.3431 13 12 13C13.6569 13 15 14.1193 15 15.5Z"
-        fill="#121212"
+        fill={color ? "#121212" : "rgba(18, 18, 18, 0.7)"}
       />
     </svg>
   );
